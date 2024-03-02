0aa0039624bf8ba2038aae988ee2511ebd2ff541
vunh195 <nguyenhoangvubt2019@gmail.com>
2024-03-02 16:08:13 +0700

    fix build failed

--- src/modules/nft-terms-of-service/nft-terms-of-service.tsx
+++
@@ -1,9 +0,0 @@
-import { Wrapper } from './terms-of-service.styled';
-
-export const TermsOfService = () => {
-  return (
-    <Wrapper>
-      <div className='terms-of-service'></div>
-    </Wrapper>
-  );
-};
