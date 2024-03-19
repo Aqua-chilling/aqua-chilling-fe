f6db9f336fc1e21bf3bd625a079a29efb2e5f047
vunh195 <nguyenhoangvubt2019@gmail.com>
2024-03-19 19:19:59 +0700

    temp login social

--- src/components/oauth-google-success/oauth-google-sucess.tsx
+++ src/components/oauth-google-success/oauth-google-sucess.tsx
@@ -11,6 +11,8 @@ export const OauthGoogleSuccess = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const google_code = searchParams.get('google_code');
+  const twitter_code = searchParams.get('twitter_code');
+  const discord_code = searchParams.get('discord_code');
   const { addNotification } = useNotification();
   React.useEffect(() => {
     if (google_code) {
@@ -38,7 +40,56 @@ export const OauthGoogleSuccess = () => {
           });
         });
     }
-  }, [google_code]);
-  console.log(google_code);
+    if (twitter_code) {
+      OauthRepository.loginByX(twitter_code)
+        .then((rs) => {
+          addNotification({
+            message: 'Login successfully',
+            type: NOTIFICATION_TYPE.SUCCESS,
+            id: new Date().getTime()
+          });
+          dispatch(
+            updateAccount({
+              email: rs.email,
+              token: rs.token,
+              id: rs.id
+            })
+          );
+          navigate('/airdrop');
+        })
+        .catch((err) => {
+          addNotification({
+            message: err,
+            type: NOTIFICATION_TYPE.ERROR,
+            id: new Date().getTime()
+          });
+        });
+    }
+    if (discord_code) {
+      OauthRepository.loginByDiscord(discord_code)
+        .then((rs) => {
+          addNotification({
+            message: 'Login successfully',
+            type: NOTIFICATION_TYPE.SUCCESS,
+            id: new Date().getTime()
+          });
+          dispatch(
+            updateAccount({
+              email: rs.email,
+              token: rs.token,
+              id: rs.id
+            })
+          );
+          navigate('/airdrop');
+        })
+        .catch((err) => {
+          addNotification({
+            message: err,
+            type: NOTIFICATION_TYPE.ERROR,
+            id: new Date().getTime()
+          });
+        });
+    }
+  }, [google_code, twitter_code, discord_code]);
   return <></>;
 };
