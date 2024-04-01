b9f65cc4d962c48eb6bd0ec8a424b3d3764a2b53
truonggiang-univer <55015029+truonggiang-univer@users.noreply.github.com>
2024-04-01 20:35:59 +0700

    feature: update package

---
+++ src/modules/packages/components/purchase-card.tsx
@@ -0,0 +1,18 @@
+import Card1 from '@/assets/packages/cards/card1.png';
+import Card2 from '@/assets/packages/cards/card2.png';
+export const PurchaseCard = () => {
+  return (
+    <div className='purchase-cards'>
+      <div className='purchase-card'>
+        <div className='card-amount'>2x Package 1</div>
+        <img src={Card1} alt='' />
+        <div className='card-btn'>Open</div>
+      </div>
+      <div className='purchase-card'>
+        <div className='card-amount'>2x Package 1</div>
+        <img src={Card2} alt='' />
+        <div className='card-btn'>Open</div>
+      </div>
+    </div>
+  );
+};
