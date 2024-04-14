2c82f8b6234dbf61d5a688d88f64c4ef2e19729d
DESKTOP\truon <truonggiang12c4b@gmail.com>
2024-04-15 01:40:43 +0700

    fix: buffer erorr

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import path from 'path';
+import inject from '@rollup/plugin-inject';
 
 export default defineConfig({
   plugins: [react()],
@@ -18,16 +19,21 @@ export default defineConfig({
   },
   optimizeDeps: {
     esbuildOptions: {
-        // Node.js global to browser globalThis
-        define: {
-            global: 'globalThis'
-        },
-        // Enable esbuild polyfill plugins
-        plugins: [
-            NodeGlobalsPolyfillPlugin({
-                buffer: true
-            })
-        ]
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis'
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true
+        })
+      ]
     }
-}
+  },
+  build: {
+    rollupOptions: {
+      plugins: [inject({ Buffer: ['buffer/', 'Buffer'] })]
+    }
+  }
 });
